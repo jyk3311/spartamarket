2505414e8ca575442174bb7808b17ea696f5f6c8
unknown <pigking41@gmail.com>
2024-08-20 11:17:31 +0900

    feat: login 기능 생성 accounts/templates/login.html 생성 - login.html 에 csrf_token 생성, form.as_p 생성, 로그인 button 생성 accounts/urls.py/login path 생성 accounts/views.py/login 함수 로직 생성 - GET, POST 만 받을수 있게 @require_http_methods(['GET', 'POST']) - 조건문을 사용해서 만약 POST로 데이터가 들어올 경우(form =   AuthenticationForm(data=request.POST))   form.is_valid()(유효성검사)를 거친 후 next 페이지 또는 homepage로   이동하게 설정

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('homepage/', views.homepage, name="homepage"), # 홈페이지 urlpatterns
     path('sign-up/', views.sign_up, name="sign_up"), # 회원가입 urlpatterns
+    path('login/', views.login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 
 # Create your views here.
@@ -12,3 +14,19 @@ def sign_up(request):
     # if request.method == 'POST':
     # form = CustomUserCreationForm(request.POST)
     return render(request, "accounts/sign_up.html")
+
+
+
+@require_http_methods(['GET', 'POST']) # GET, POST 만 받을수 있게 설정
+def login(request): # 로그인
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_url = request.GET.get('next') or 'accounts:homepage'
+            return redirect(next_url)
+    else:
+        form = AuthenticationForm()
+    
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
