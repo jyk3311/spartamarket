b8c10805f9774bb7a9c7a6b095c139679b62bd67
unknown <pigking41@gmail.com>
2024-08-20 12:17:43 +0900

    feat: logout 구현 - accounts/views.py logout 함수 로직 생성 accounts/urls.py logout path 생성 base.html에 로그아웃 버튼 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('homepage/', views.homepage, name="homepage"), # 홈페이지 urlpatterns
     path('sign-up/', views.sign_up, name="sign_up"), # 회원가입 urlpatterns
-    path('login/', views.login, name="login"),
+    path('login/', views.login, name="login"), # 로그인
+    path('logout/', views.logout, name="logout"), # 로그아웃
 ]
