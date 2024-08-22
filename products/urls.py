b3a82fe865472c0eca63b044c6287ed4b66472cf
namjimin3123832 <mimi3123832@gmail.com>
2024-08-22 11:03:29 +0900

    templates 폴더 안에 post_detail.html 파일 생성 - urls.py에 post_detail을 path로 작성 - views.py에  게시 물건 디테일 내용 임시 작성 - post_detail.html 파일 임시 작성

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 # namespace = 같은이름의 파일이 있을때 사용
 app_name = 'products'
 urlpatterns = [
+    path("post_detail/", views.post_detail, name = "post_detail"),  # 게시 물건 디테일 urlpatterns
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_detail(request):
+	id = "지민"
+	town = "스파르타 주민"
+	title = " 애플 펜슬 2세대 팝니다!"
+	price = "120000"
+	detail = "개봉하고 한 번 밖에 사용 안해서 깨끗해요."
+	
+	
+	context = {
+		"id": id,
+		"town": town,
+		"title": title,
+		"price": price,
+		"detail": detail,
+
+		}
+	return render(request, "products/post_detail.html", context)
+
+
