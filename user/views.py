c51031b5f160eb2bcb89f1ac1531ffdd57ef2dd9
unknown <pigking41@gmail.com>
2024-08-23 16:10:55 +0900

    feat: 회원정보변경 기능 및 글 목록 이동 구현
    - 홈페이지에서 회원정보변경기능 및 글 목록으로 이동하는 기능 구현
    - 두개다 로그인한 상태에서 가능하게 구현

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from user.temp.forms import ArticleForm
 
 # @login_required # 로그인을 해야만 화면이 보임.
 # def user(request):
@@ -8,9 +9,11 @@ from django.contrib.auth import get_user_model
 #     items = user.items.all()  # 유저가 등록한 물품들 불러오기
 #     return render(request, 'user.html', {'user': user, 'items': items})
 
+
 def profile(request, username):
     member = get_object_or_404(get_user_model(), username = username) # db 탐색 후, 회원이 있는지 없는지 확인하는 작업 ##
     context = {
-        "member" : member
+        "member" : member,
     }
     return render(request,'user/profile.html', context)
+
